90df7af1d5de6581d602089f6a7059255bd494cb
mewqwem <roadto12mmr@gmail.com>
2025-10-04 09:33:47 +0200

    lab1
    lab1

---
+++ WebApplication1/WebApplication1/Controllers/BlogController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly ILogger<BlogController> _logger;
+
+        public BlogController(ILogger<BlogController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
